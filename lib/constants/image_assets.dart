411c4912a49575ff936afc34045e86bbcd76ede7
hungf1324 <hungf1324@gmail.com>
2022-08-15 13:54:33 +0700

    FlexDemoPage

--- lib/constants/image_assets.dart
+++ lib/constants/image_assets.dart
@@ -2,4 +2,7 @@
 
 class ImageAssets {
   static String catImg = "assets/images/sasa.jpg";
+  static String flutterBanner = "assets/images/banners/flutter.jpg";
+  static String androidBanner = "assets/images/banners/android.jpg";
+  static String javaBanner = "assets/images/banners/java.png";
 }
--- lib/pages/flexdemo_page.dart
+++ lib/pages/flexdemo_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:learning_flutter/constants/image_assets.dart';
+import 'package:learning_flutter/styles/thick_divider.dart';
 
 class FlexDemoPage extends StatelessWidget {
   const FlexDemoPage({Key? key}) : super(key: key);
@@ -6,6 +8,105 @@ class FlexDemoPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    String title = 'Flex Demo - CodeFresher';
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(title),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              FlexBanner(
+                title: 'Lập trình Flutter',
+                subTitle: 'Thực chiến dự án app mobile 2022',
+                image: Image.asset(ImageAssets.flutterBanner),
+              ),
+              const ThickDivider(),
+              FlexBanner(
+                title: 'Lập trình Android',
+                subTitle: 'Java + Kotlin',
+                image: Image.asset(ImageAssets.androidBanner),
+                rightImage: true,
+              ),
+              const ThickDivider(),
+              FlexBanner(
+                title: 'Lập trình Java cơ bản',
+                subTitle: 'Cho người mới bắt đầu',
+                image: Image.asset(ImageAssets.javaBanner),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class FlexBanner extends StatelessWidget {
+  const FlexBanner({
+    Key? key,
+    required this.title,
+    this.titleSize = 32,
+    required this.subTitle,
+    this.subTitleSize = 24,
+    required this.image,
+    this.rightImage = false,
+    this.flex = 1,
+  }) : super(key: key);
+
+  final String title;
+  final double titleSize;
+  final String subTitle;
+  final double subTitleSize;
+  final Image image;
+  final bool rightImage;
+  final int flex;
+
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
+      flex: flex,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          if (!rightImage)
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: image,
+            ),
+          Flexible(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  title,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: titleSize,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Text(
+                  subTitle,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: subTitleSize,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          if (rightImage)
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: image,
+            ),
+        ],
+      ),
+    );
   }
 }
